1595f8e94f258a4b8f90444e003c5ce88b284155
marsanbla <a21marsanbla@inspedralbes.cat>
2022-12-13 09:59:33 +0100

    f

--- app/src/main/java/com/example/p2sanmar/MainActivity.java
+++ app/src/main/java/com/example/p2sanmar/MainActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void readprod(View view) {
-        persistenciaprod read = new persistenciaprod();
-        persistenciaprod.readprod();
+        ArrayList<prods> productsArray = persistenciaprod.readprod();
+       prod1.setText
     }
 }
